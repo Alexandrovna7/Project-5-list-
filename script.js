0c55215b9ff58aacb1636d9d19fcbc13a39354d6
Alexandrovna7 <dina25alexandrovna05@gmail.com>
2025-03-25 12:45:20 +0300

    js

---
+++ script.js
@@ -0,0 +1,39 @@
+/* Новые элементы должны добавляться в список по нажатию на Enter */
+
+/* Пустые элементы не должны добавляться */
+
+/* Если кликнуть на элемент списка, он зачеркивается */
+
+/* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
+
+/* Очищать input после добавления нового элемента в список */
+
+
+
+
+const input = document.querySelector("#input");
+const allItems = document.querySelector("#items");
+
+input.addEventListener("keydown", function(event) {
+    if (event.key == "Enter") {
+        const item = document.createElement("h2");
+        item.textContent = input.value;
+
+    item.addEventListener("click", function () {
+        item.classList.toggle("done");
+    });
+
+    if (input.value != "") {
+        allItems.appendChild(item);
+        }
+
+        input.value = "";
+    }
+    });
+
+
+
+
+
+
+
